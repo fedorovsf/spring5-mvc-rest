95e78b172b300215d09d2355ce5e773490000050
Sergei Fedorov <fedorovs@inbox.ru>
2019-01-09 14:56:53 +0300

    refactoring for multi module maven project

---
+++ spring-mvc-rest-app/src/main/java/guru/springframework/api/v1/model/CategoryListDTO.java
@@ -0,0 +1,15 @@
+package guru.springframework.api.v1.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.util.List;
+
+
+@Data
+@AllArgsConstructor
+public class CategoryListDTO {
+
+    List<CategoryDTO> categories;
+
+}
